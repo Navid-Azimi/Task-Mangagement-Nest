279ef6ef954e94ab5674497237394e3df1441e4d
Sayed Navid <navidjan.azimi132@gmail.com>
2025-04-17 16:22:14 +0430

    create task controller and service dto is passed

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Post } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,7 +12,8 @@ export class TasksService {
   }
 
   @Post()
-  createTask(title: string, description: string): Task {
+  createTask(createtaskDto: CreateTaskDto): Task {
+    const { title, description } = createtaskDto;
     const task: Task = {
       id: uuid(),
       title,
