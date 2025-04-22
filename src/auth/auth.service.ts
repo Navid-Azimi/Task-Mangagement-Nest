4199642d0d3f999b863458b6ced1f43a33a87fd5
Sayed Navid <navidjan.azimi132@gmail.com>
2025-04-22 13:52:51 +0430

    error handling sign up is done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(@Inject('UsersRepository') private usersRepo: UsersRepository) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return await this.usersRepo.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,11 +1,29 @@
 // users.repository.ts
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
 
-  //
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // Duplicate Entry
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
